559e80450a0bf28668ea93530ba1d813c2303eb3
marleen kovalik <marleen.kovalik@khk.ee>
2019-11-11 11:35:00 +0200

    omadused

--- app.js
+++ app.js
@@ -5,13 +5,29 @@
 //alert('Tere hommikust!');
 
 // prompt
-const sisend = prompt('Sisesta oma nimi');
-alert('Tere hommikust, ' + sisend + '!');
+//const sisend = prompt('Sisesta oma nimi');
+//alert('Tere hommikust, ' + sisend + '!');
 
 // confirm
-if(confirm('Kas oled nõus?')){
-    console.log('jah');
-} else {
-    console.log('ei');
-}
+//if(confirm('Kas oled nõus?')){
+//    console.log('jah');
+//} else {
+//    console.log('ei');
+//}
 
+// välimõõdud
+let valiKorgus = window.outerHeight;
+let valiLaius = window.outerWidth;
+//console.log(valiKorgus);
+//console.log(valiLaius);
+
+// sisemõõdud
+let siseKorgus = window.innerHeight;
+let siseLaius = window.innerWidth;
+//console.log(siseKorgus);
+//console.log(siseLaius);
+
+// scroll
+scrolY = window.scrollY;
+scrolX = window.scrollX;
+console.log(scrolX);
