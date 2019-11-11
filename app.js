efd21572267a7f6d5a30e0c84ff31c01905c1cd5
marleen kovalik <marleen.kovalik@khk.ee>
2019-11-11 11:23:41 +0200

    funktsioonid

---
+++ app.js
@@ -0,0 +1,17 @@
+// window objects / methods / properties
+
+// alert 
+//window.alert('Tere hommikust!');
+//alert('Tere hommikust!');
+
+// prompt
+const sisend = prompt('Sisesta oma nimi');
+alert('Tere hommikust, ' + sisend + '!');
+
+// confirm
+if(confirm('Kas oled nõus?')){
+    console.log('jah');
+} else {
+    console.log('ei');
+}
+
