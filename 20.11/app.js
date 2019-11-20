f9bc32bd853bbbc9acf4a8aae66b1b62b78c8604
marleen kovalik <marleen.kovalik@khk.ee>
2019-11-20 09:23:50 +0200

    event objecti omadused

--- 20.11/app.js
+++ 20.11/app.js
@@ -1,13 +1,29 @@
 // sündmused
 
-const kustutaKoik = document.querySelector
-('.clear-tasks');
-kustutaKoik.addEventListener('click', 
-vajutaNupp); 
+const kustutaKoik = document.querySelector('.clear-tasks');
+kustutaKoik.addEventListener('click', vajutaNupp); 
 
 function vajutaNupp(e){
-    console.log('vajutatud');
-    // e.preventDefault();
+    let sisu = e;
+    // target
+    sisu = e.target;
+    sisu = e.target.id;
+    sisu = e.target.className;
+    sisu = e.target.classList;
+
+    // tüüp
+    sisu = e.type;
+
+    // sündmuse koordinaadid browseri akna suhtes
+    sisu = e.clientY
+    sisu = e.clientX
+    console.log(sisu);
+
+        // sündmuse koordinaadid elemendi suhtes
+        sisu = e.offsetY
+        sisu = e.offsetX
+        console.log(sisu);
 }
 
+  
 
