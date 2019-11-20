22ba746ef12943e589059cf0a46faec57b446763
marleen kovalik <marleen.kovalik@khk.ee>
2019-11-20 08:45:34 +0200

    20.11

--- Uus kaust/app.js
+++ Uus kaust/app.js
@@ -11,3 +11,14 @@ document.querySelector('ul li').style.color = 'blue';
 document.querySelector('li:last-child').style.color = 'red';
 document.querySelector('li:nth-child(2)').style.color = 'orange';
 console.log(sisu);
+
+// multiply elements selectors
+
+let sisu = document.getElementsByClassName('collection-item');
+console.log(sisu);
+console.log(sisu 0 );
+sisu[0].style.color = 'red';
+sisu[2].textContent = 'Õpi Arvutivõrgud';
+
+const nimekirjaElemendid = document.querySelector('ul');
+console.log(nimekirjaElemendid);
